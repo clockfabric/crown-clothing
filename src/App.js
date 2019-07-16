65b53407cab77f97a10693843b55ed56590cefd5
test <test>
2019-07-16 14:58:21 -0400

    Added React Router Dom functionality to our project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import HomePage from "./pages/homepage/HomePage";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
 	return (
 		<React.Fragment>
-			<HomePage />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				{/* <Route exact path='/hats' component={HatsPage} /> */}
+			</Switch>
 		</React.Fragment>
 	);
 }
