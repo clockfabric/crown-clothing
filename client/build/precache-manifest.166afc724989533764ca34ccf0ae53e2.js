f4af958a736bca5988096cc45c83f9a1c26f2814
test <test>
2019-08-12 15:21:13 -0400

    Created node server with express to implement Stripe checkout, moved all the client code into /client folder

---
+++ client/build/precache-manifest.166afc724989533764ca34ccf0ae53e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8203a5297330693f7ac7efb5063a86d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88015a938f50989017aa",
+    "url": "/static/css/main.26112965.chunk.css"
+  },
+  {
+    "revision": "2edd1a921ab19de96f52",
+    "url": "/static/js/2.3af4625a.chunk.js"
+  },
+  {
+    "revision": "88015a938f50989017aa",
+    "url": "/static/js/main.66106ef3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- client/src/components/stripe-button/StripeButton.js
+++ client/src/components/stripe-button/StripeButton.js
@@ -1,13 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
 	const priceForStripe = price * 100;
 	const publishableKey = "pk_test_sUZLW06l8BnJ88hG0PVdfLA30008tn0aj0";
 
 	const onToken = token => {
-		console.log(token);
-		alert("Payment Successful");
+		axios({
+			url: "payment",
+			mathod: "post",
+			data: {
+				amount: priceForStripe,
+				token: token
+			}
+		})
+			.then(response => {
+				alert("Payment successfull");
+			})
+			.catch(error => {
+				console.log("Payment error: ", error);
+				alert(
+					"There was an issue with your payment. Please make sure you use the provided credit card."
+				);
+			});
 	};
 
 	return (
